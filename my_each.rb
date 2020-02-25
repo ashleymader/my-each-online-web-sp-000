997bf6da86e08825db4a39d35b272077bf19479a
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-25 13:19:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,3 +4,15 @@ def my_each(collection, index)
   while index
   return (my_each)collection
 end
+
+def hello_t(array)
+  if block_given?
+  i = 0 
+  while i < array.length 
+    yield (array[i])
+    i = i + 1 
+  end
+    array
+  else puts "Hey! No block was given!"
+  end
+end
